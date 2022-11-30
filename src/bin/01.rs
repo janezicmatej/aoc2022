000000102ce2d0cac2d088a9d392d74852f480a8
Matej Janežič <matej.janezic@aflabs.si>
2022-11-30 22:05:41 +0100

    chore: prepare files for day1

---
+++ src/bin/01.rs
@@ -0,0 +1,25 @@
+pub fn part_one(input: &str) -> Option<u32> {
+    None
+}
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+fn main() {
+    let input = &aoc::read_file("inputs", 1);
+    aoc::solve!(1, part_one, input);
+    aoc::solve!(2, part_two, input);
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_part_one() {
+        let input = aoc::read_file("test_inputs", 1);
+        assert_eq!(part_one(&input), None);
+    }
+    #[test]
+    fn test_part_two() {
+        let input = aoc::read_file("test_inputs", 1);
+        assert_eq!(part_two(&input), None);
+    }
+}
