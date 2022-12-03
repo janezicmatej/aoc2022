00000080e2ab8f9a113d3eaa30ec65ebfedfde79
Matej Janežič <matej.janezic@aflabs.si>
2022-12-03 08:05:19 +0100

    solution: day3

---
+++ src/bin/03.rs
@@ -0,0 +1,62 @@
+use std::ops::Not;
+
+use aoc::helpers::ASCII;
+use hashbrown::{HashMap, HashSet};
+use itertools::Itertools;
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut score = 0;
+    for rucksack in input.trim().split('\n') {
+        let compartment_size = rucksack.len() / 2;
+        let set: HashSet<char> = rucksack[..compartment_size].chars().collect();
+        for item in rucksack[compartment_size..].chars() {
+            if set.contains(&item) {
+                score += 1 + ASCII.iter().position(|&x| x == item).unwrap() as u32;
+                break;
+            }
+        }
+    }
+    Some(score)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut score = 0;
+    let mut map = HashMap::with_capacity(ASCII.len());
+    let mut set = HashSet::with_capacity(ASCII.len());
+
+    for rucksacks in input.trim().split('\n').collect_vec().chunks(3) {
+        map.clear();
+        for rucksack in rucksacks.iter() {
+            set.clear();
+            for c in rucksack.chars() {
+                if set.contains(&c).not() {
+                    *map.entry(c).or_insert(0) += 1;
+                    set.insert(c);
+                };
+            }
+        }
+        map.retain(|_, &mut v| v == 3);
+        let common_item = map.keys().next().unwrap();
+        score += 1 + ASCII.iter().position(|&x| x == *common_item).unwrap() as u32;
+    }
+    Some(score)
+}
+fn main() {
+    let input = &aoc::read_file("inputs", 3);
+    aoc::solve!(1, part_one, input);
+    aoc::solve!(2, part_two, input);
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_part_one() {
+        let input = aoc::read_file("test_inputs", 3);
+        assert_eq!(part_one(&input), Some(157));
+    }
+    #[test]
+    fn test_part_two() {
+        let input = aoc::read_file("test_inputs", 3);
+        assert_eq!(part_two(&input), Some(70));
+    }
+}
--- src/helpers.rs
+++ src/helpers.rs
@@ -1,3 +1,4 @@
+use lazy_static::lazy_static;
 use std::{fmt::Debug, str::FromStr};
 
 pub fn to_vec<T>(input: &str, split: char) -> Vec<T>
@@ -24,4 +25,20 @@ where
                 .collect()
         })
         .collect()
+}
+
+lazy_static! {
+    pub static ref ASCII_LOWERCASE: [char; 26] = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+        's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    ];
+    pub static ref ASCII_UPPERCASE: [char; 26] = [
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    ];
+    pub static ref ASCII: [char; 52] = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+        's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+        'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    ];
 }
