00000150fbb79314980a62624c1ddbb3f132b44b
Matej Janežič <matej.janezic@aflabs.si>
2022-12-05 07:27:27 +0100

    solution: day5

---
+++ src/bin/05.rs
@@ -0,0 +1,100 @@
+use itertools::Itertools;
+use lazy_static::lazy_static;
+use regex::Regex;
+
+lazy_static! {
+    static ref RE: Regex = Regex::new(r#"move (\d+) from (\d+) to (\d+)"#).unwrap();
+    static ref SKIP: Vec<char> = vec!['[', ' ', ']'];
+}
+
+fn parse_towers(text_towers: &str) -> Vec<Vec<char>> {
+    let mut rows = text_towers.split('\n').rev();
+    let num_towers = rows.next().unwrap().trim().split("   ").count();
+    let mut towers = Vec::new();
+    for _ in 0..num_towers {
+        towers.push(Vec::new());
+    }
+
+    for row in rows {
+        for (i, c) in row.chars().enumerate() {
+            if !SKIP.contains(&c) {
+                towers[i / 4].push(c);
+            }
+        }
+    }
+
+    towers
+}
+
+pub fn part_one(input: &str) -> Option<String> {
+    let (text_towers, instructions) = input.split("\n\n").next_tuple().unwrap();
+    let mut towers = parse_towers(text_towers);
+
+    for line in instructions.trim().split('\n') {
+        let captures = RE.captures(line).unwrap();
+
+        let (amount, from, to): (usize, usize, usize) = (
+            captures[1].parse().unwrap(),
+            captures[2].parse().unwrap(),
+            captures[3].parse().unwrap(),
+        );
+
+        let mut aux = Vec::new();
+        for _ in 0..amount {
+            let aaux = towers[from - 1].pop().unwrap();
+            aux.push(aaux);
+        }
+
+        towers[to - 1].append(&mut aux);
+    }
+
+    Some(String::from_iter(
+        towers.iter().map(|x| x.iter().rev().next().unwrap()),
+    ))
+}
+pub fn part_two(input: &str) -> Option<String> {
+    let (text_towers, instructions) = input.split("\n\n").next_tuple().unwrap();
+    let mut towers = parse_towers(text_towers);
+
+    for line in instructions.trim().split('\n') {
+        let captures = RE.captures(line).unwrap();
+
+        let (amount, from, to): (usize, usize, usize) = (
+            captures[1].parse().unwrap(),
+            captures[2].parse().unwrap(),
+            captures[3].parse().unwrap(),
+        );
+
+        let mut aux = Vec::new();
+        for _ in 0..amount {
+            let aaux = towers[from - 1].pop().unwrap();
+            aux.push(aaux);
+        }
+        aux.reverse();
+
+        towers[to - 1].append(&mut aux);
+    }
+
+    Some(String::from_iter(
+        towers.iter().map(|x| x.iter().rev().next().unwrap()),
+    ))
+}
+fn main() {
+    let input = &aoc::read_file("inputs", 5);
+    aoc::solve!(1, part_one, input);
+    aoc::solve!(2, part_two, input);
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_part_one() {
+        let input = aoc::read_file("test_inputs", 5);
+        assert_eq!(part_one(&input), Some("CMZ".to_string()));
+    }
+    #[test]
+    fn test_part_two() {
+        let input = aoc::read_file("test_inputs", 5);
+        assert_eq!(part_two(&input), Some("MCD".to_string()));
+    }
+}
