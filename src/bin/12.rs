0000043046124a42feff0b3d60f1b8471d0fda44
Matej Janežič <matej.janezic@aflabs.si>
2022-12-17 23:09:27 +0100

    solution: day12 optimize with single reverse bfs

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,6 +1,6 @@
 use std::collections::VecDeque;
 
-use hashbrown::HashSet;
+use hashbrown::HashMap;
 use itertools::Itertools;
 
 struct Input {
@@ -18,15 +18,15 @@ fn parse_input(input: &str) -> Input {
 
     let (sx, sy) = (0..grid.len())
         .cartesian_product(0..grid[0].len())
-        .find(|&(x, y)| grid[x][y] == 'S')
+        .find(|&(x, y)| grid[x][y] == 'E')
         .unwrap();
     let (ex, ey) = (0..grid.len())
         .cartesian_product(0..grid[0].len())
-        .find(|&(x, y)| grid[x][y] == 'E')
+        .find(|&(x, y)| grid[x][y] == 'S')
         .unwrap();
 
-    grid[sx][sy] = 'a';
-    grid[ex][ey] = 'z';
+    grid[sx][sy] = 'z';
+    grid[ex][ey] = 'e';
 
     Input {
         grid,
@@ -34,42 +34,41 @@ fn parse_input(input: &str) -> Input {
         end: (ex, ey),
     }
 }
-fn bfs(grid: &[Vec<char>], start: (usize, usize), end: (usize, usize)) -> Option<u32> {
+fn bfs(grid: &[Vec<char>], start: (usize, usize)) -> HashMap<(usize, usize), u32> {
     let mut queue = VecDeque::new();
-    let mut visited = HashSet::new();
+    let mut visited = HashMap::new();
     queue.push_back((start, 0));
-    visited.insert(start);
+    visited.insert(start, 0);
     while !queue.is_empty() {
         let ((x, y), c) = queue.pop_front().unwrap();
-        if (x, y) == end {
-            return Some(c);
-        }
 
         for (dx, dy) in [(1, 0), (-1, 0), (0, 1), (0, -1)] {
             let (nx, ny) = ((x as isize + dx) as usize, (y as isize + dy) as usize);
             let Some(&next) = grid.get(nx).and_then(|row| row.get(ny)) else {continue;};
 
-            if grid[x][y] as u8 + 1 >= next as u8 && !visited.contains(&(nx, ny)) {
-                visited.insert((nx, ny));
+            if grid[x][y] as u8 <= next as u8 + 1 && !visited.contains_key(&(nx, ny)) {
+                visited.insert((nx, ny), c + 1);
                 queue.push_back(((nx, ny), c + 1));
             }
         }
     }
-    None
+    visited
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
     let Input { grid, start, end } = parse_input(input);
-    bfs(&grid, start, end)
+    bfs(&grid, start).get(&end).copied()
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let Input { grid, end, .. } = parse_input(input);
+    let Input { grid, start, .. } = parse_input(input);
+    let visited = bfs(&grid, start);
     (0..grid.len())
         .cartesian_product(0..grid[0].len())
         .filter(|&(x, y)| grid[x][y] == 'a')
-        .filter_map(|start| bfs(&grid, start, end))
+        .filter_map(|end| visited.get(&end))
         .min()
+        .copied()
 }
 fn main() {
     let input = &aoc::read_file("inputs", 12);
